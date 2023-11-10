2ba83b93470021f9b42b3c8b2f5bc4aa70c52b0a
Artem Ravbetsky <ravbetsky@gmail.com>
2023-11-10 12:54:34 +0300

    fix bd

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -2,10 +2,29 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../../shared/ui/button/Button";
 import { Logo } from "../../shared/ui/logo/Logo";
 import useLocalStorage from "react-use-localstorage";
+import { db } from "../../firebase-config";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export function MainScreen() {
-  const [playerID] = useLocalStorage("playerID");
+  const [playerID, setPlayerID] = useLocalStorage("playerID");
+  const [isLogined, setIsLogined] = useState(!!playerID);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const getPlayer = async () => {
+      const playerDocRef = doc(db, "players", playerID);
+      const player = await getDoc(playerDocRef);
+      if (!player.exists()) {
+        setPlayerID("");
+        setIsLogined(false);
+      } else {
+        setIsLogined(true);
+      }
+    };
+    getPlayer();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="cover">
       <div className="main-top-ui">
@@ -13,7 +32,7 @@ export function MainScreen() {
       </div>
       <div className="drawer-ui">
         <Button title="Лидерборд" onClick={() => navigate("/leaderboard")} />
-        {playerID && (
+        {isLogined   && (
           <Button title="Настройки" onClick={() => navigate("/settings")} />
         )}
         <Button
